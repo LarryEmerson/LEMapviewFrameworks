e6362d18dee635c25ad1142248d8cc2cab243339
LarryEmerson <LarryEmerson@e-MacBook-Pro.lan>
2016-09-01 16:45:20 +0800

    pod update

--- Example/Pods/LEFrameworks/LEFrameworks/Classes/LEBase/LEBaseViewController.h
+++ Example/Pods/LEFrameworks/LEFrameworks/Classes/LEBase/LEBaseViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "LEUIFramework.h" 
-
+#import "LEUIFrameworkExtra.h"
 @protocol LEViewControllerPopDelegate <NSObject>
 -(void) leOnViewControllerPopedWithPageName:(NSString *) order AndData:(id) data;
 @end
@@ -23,6 +23,7 @@
 @property (nonatomic, readonly) int leCurrentFrameHight;
 @property (nonatomic, readonly) UIView *leViewContainer;
 @property (nonatomic, readonly) UIView *leViewBelowCustomizedNavigation;
+@property (nonatomic, readonly) int leFrameHightForCustomizedView;
 @property (nonatomic, readonly) LEBaseViewController *leCurrentViewController;
 -(UIView *) leSuperViewContainer;
 -(id) initWithViewController:(LEBaseViewController *) vc;
@@ -43,11 +44,14 @@
     
 }
 @property (nonatomic) UIView *leTitleViewContainer;
+-(id) initWithSuperViewAsDelegate:(LEBaseView *)superview Title:(NSString *) title;
+-(id) initWithDelegate:(id<LENavigationDelegate>)delegate SuperView:(LEBaseView *)superview Title:(NSString *) title;
 -(id) initWithDelegate:(id<LENavigationDelegate>) delegate ViewController:(UIViewController *) viewController SuperView:(UIView *) superview Offset:(int) offset BackgroundImage:(UIImage *) background TitleColor:(UIColor *) color LeftItemImage:(UIImage *) left;
 -(void) leSetNavigationTitle:(NSString *) title;
--(void) leSetLeftButton:(UIImage *) img;
+//-(void) leSetLeftButton:(UIImage *) img;
 -(void) leEnableBottomSplit:(BOOL) enable Color:(UIColor *) color;
 -(void) leSetBackground:(UIImage *) image;
+-(void) leSetLeftNavigationItemWith:(NSString *)title Image:(UIImage *)image Color:(UIColor *) color;
 -(void) leSetRightNavigationItemWith:(NSString *) title Image:(UIImage *) image;
 -(void) leSetRightNavigationItemWith:(NSString *) title Image:(UIImage *) image Color:(UIColor *) color;
 -(void) leSetNavigationOffset:(int) offset;
